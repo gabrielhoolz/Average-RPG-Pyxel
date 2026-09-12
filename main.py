b4f0c92881774208176ee3e777c1dc716e85349f
gabrielhoolz <gabrielhm1501@gmail.com>
2026-09-12 14:44:47 -0300

    andamento do combate

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 import pyxel
+import random
 
 class RPG:
     def __init__(self):
-        pyxel.init(160, 120, title="Avarage RPG Pyxel")
+        pyxel.init(160, 120, title="Average RPG Pyxel")
         pyxel.mouse(True)
+        
+        try:
+            pyxel.load("rpg.pyxres")
+        except:
+            pass
+            
         self.tela = "menu"
         
         self.hp_jogador = 100
-        self.hp_max_jogador = 100 # trava para n ter sobrevida
+        self.hp_max = 100
         self.hp_chefe = 150
         self.turno = "jogador"
         
-        # pocoes defesa tempo
         self.pocoes = 3
         self.defendendo = False
+        
+        # variavel dano chefe
         self.timer_chefe = 0
+        self.dano_pendente = 0
+        self.msg_chefe = ""
         
         pyxel.run(self.update, self.draw)
 
@@ -33,51 +43,50 @@ class RPG:
             elif self.hp_jogador <= 0:
                 self.tela = "derrota"
             
-            # turno do jogador
             elif self.turno == "jogador":
                 if pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT):
                     
-                    # botao atacar
-                    if 10 <= pyxel.mouse_x <= 45 and 90 <= pyxel.mouse_y <= 105:
+                    if 10 <= pyxel.mouse_x <= 45 and 90 <= pyxel.mouse_y <= 105: # ataque
                         self.hp_chefe -= 15
                         self.defendendo = False
-                        self.turno = "chefe_espera"
-                        self.timer_chefe = 30
+                        self.turno = "chefe_decide"
                         
-                    # botao defender
-                    elif 50 <= pyxel.mouse_x <= 95 and 90 <= pyxel.mouse_y <= 105:
+                    elif 50 <= pyxel.mouse_x <= 95 and 90 <= pyxel.mouse_y <= 105: # defende
                         self.defendendo = True
-                        self.turno = "chefe_espera"
-                        self.timer_chefe = 30
+                        self.turno = "chefe_decide"
                         
-                    # botao curar
-                    elif 100 <= pyxel.mouse_x <= 140 and 90 <= pyxel.mouse_y <= 105:
+                    elif 100 <= pyxel.mouse_x <= 140 and 90 <= pyxel.mouse_y <= 105: # cura
                         if self.pocoes > 0:
                             self.hp_jogador += 30
-                            # limite vida
-                            if self.hp_jogador > self.hp_max_jogador:
-                                self.hp_jogador = self.hp_max_jogador
-                            
+                            if self.hp_jogador > self.hp_max:
+                                self.hp_jogador = self.hp_max
                             self.pocoes -= 1
                             self.defendendo = False
-                            self.turno = "chefe_espera"
-                            self.timer_chefe = 30
+                            self.turno = "chefe_decide"
                         
-            # turno chefe tempo
+            # sorteio do chefe
+            elif self.turno == "chefe_decide":
+                chance = random.randint(1, 100)
+                
+                if chance <= 20: 
+                    self.dano_pendente = 25
+                    self.msg_chefe = "ATAQUE CRITICO!"
+                else: 
+                    self.dano_pendente = random.randint(8, 12)
+                    self.msg_chefe = "O Chefe ataca!"
+                
+                if self.defendendo:
+                    self.dano_pendente = self.dano_pendente // 2 
+                    
+                self.timer_chefe = 45
+                self.turno = "chefe_espera"
+                    
+            # msg fica ate toma odano
             elif self.turno == "chefe_espera":
                 self.timer_chefe -= 1
                 if self.timer_chefe <= 0:
-                    self.turno = "chefe_ataque"
-                    
-            # turno chefe dano
-            elif self.turno == "chefe_ataque":
-                dano_chefe = 10
-                
-                if self.defendendo:
-                    dano_chefe = 5 
-                    
-                self.hp_jogador -= dano_chefe
-                self.turno = "jogador"
+                    self.hp_jogador -= self.dano_pendente
+                    self.turno = "jogador"
 
     def draw(self):
         pyxel.cls(0)
@@ -91,26 +100,26 @@ class RPG:
             pyxel.text(100, 5, f"Chefe HP: {self.hp_chefe}", 8)
             pyxel.text(5, 15, f"Pocoes: {self.pocoes}", 10)
             
+            pyxel.blt(64, 30, 0, 0, 0, 32, 32, 0)
+            
             if self.turno == "jogador":
-                # desenho atacar
                 pyxel.rect(10, 90, 35, 15, 5)
                 pyxel.text(14, 95, "ATACAR", 7)
                 
-                # desenho defesa
                 pyxel.rect(50, 90, 45, 15, 13)
                 pyxel.text(56, 95, "DEFENDER", 7)
                 
-                # desenho curar
                 if self.pocoes > 0:
                     pyxel.rect(100, 90, 35, 15, 11)
                     pyxel.text(106, 95, "CURAR", 7)
                 else:
-                    # desliga se acaba
                     pyxel.rect(100, 90, 35, 15, 13)
                     pyxel.text(106, 95, "VAZIO", 7)
                 
             elif self.turno == "chefe_espera":
-                pyxel.text(40, 50, "O Chefe ataca!", 9)
+                # diferença nas cor da msg
+                cor_msg = 8 if self.msg_chefe == "ATAQUE CRITICO!" else 9
+                pyxel.text(55, 15, self.msg_chefe, cor_msg)
 
         elif self.tela == "vitoria":
             pyxel.text(50, 50, "VOCE VENCEU!", 11)
